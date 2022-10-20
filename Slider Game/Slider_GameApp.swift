699fdf877321adb8c1f8f0c1ff1cef4d9bbb8d72
Oleg Fedotov <trevor706@gmail.com>
2022-10-20 18:27:15 +0300

    Initial Commit

---
+++ Slider Game/Slider_GameApp.swift
@@ -0,0 +1,17 @@
+//
+//  Slider_GameApp.swift
+//  Slider Game
+//
+//  Created by Olegio on 20.10.2022.
+//
+
+import SwiftUI
+
+@main
+struct Slider_GameApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
